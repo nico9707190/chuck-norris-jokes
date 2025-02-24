547efacbd4335e2b53463f31a740919960d273b8
nico9707190 <marianinicolo123@gmail.com>
2025-02-24 08:23:06 +0100

    v1.1 ("all" categorie works)

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,28 @@
 let button = document.querySelector("#button")
 button.addEventListener("click", () => {
     let c = document.querySelector("#categorie").value
-    let joke = fetch("https://api.chucknorris.io/jokes/random?category=" + c).then(
-        function(response){
+    let l = "https://api.chucknorris.io/jokes/random";
+
+    if (c !== "all") {
+        l = l + "?category=" + c
+    }
+
+    let joke = fetch(l).then(
+        function (response) {
             console.log(response)
             return response.json();
         }
     ).then(
-        function(data){
+        function (data) {
             console.log(data)
             document.querySelector("h2").innerHTML = data.value
         }
     ).catch(
-        function(error){
+        function (error) {
             console.log(error)
         }
     )
     console.log(joke)
 })
+
+
